ff1be8b49e13ee449a02675743df407c8ca9ce18
Yogiistianto <yogiskarton@gmail.com>
2024-11-08 23:08:27 +0700

    week11

---
+++ week11/matematika.py
@@ -0,0 +1,10 @@
+PI = 3.14159
+
+def hitung_luas_lingkaran(jari_jari):
+    return PI * jari_jari * jari_jari
+
+def hitung_luas_persegi(sisi):
+    return sisi * sisi
+
+def hitung_luas_persegi_panjang(panjang, lebar):
+    return panjang * lebar
